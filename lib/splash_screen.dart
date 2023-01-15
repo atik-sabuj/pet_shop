627f8f1e9028eab305f470b9ec6584b2e8ed8b45
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-01-15 15:59:45 +0600

    Initial commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 30),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                SecondScreen()
+            )
+        )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.deepPurple,
+      child:
+      Image.asset("images/logo.png",height: 30,width: 30,),
+    );
+  }
+}
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text("GeeksForGeeks")),
+      body: Center(
+          child:Text("Home page",textScaleFactor: 2,)
+      ),
+    );
+  }
+}
