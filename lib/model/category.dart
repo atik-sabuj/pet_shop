40bc6926f4cc7c3560cc2c8558da311c7dbfe3d3
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-08 22:51:49 +0600

    updates designs #4

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -22,6 +22,8 @@ final List<Category> categoryList = [
   Category(name: "Dog",image: "images/dog logo.png"),
   Category(name: "Bird",image: "images/bird logo.png"),
   Category(name: "Rabbit",image: "images/rabbit.png"),
+  Category(name: "Fish",image: "images/fish.png"),
+  Category(name: "Duck",image: "images/duck.png"),
   Category(name: "Others",image: "images/others.png"),
 
 ];
--- lib/model/features.dart
+++ lib/model/features.dart
@@ -31,11 +31,11 @@ class Features {
 
 final List<Features> featuresList = [
   Features(name: "Cat",image: "images/cat1.png", price: 5000.0, location: "Mdpur", age: "2 years", isFavorite: true),
-  Features(name: "Dog",image: "images/dog.png", price: 8000.0, location: "Mdpur", age: "3 years", isFavorite: false),
-  Features(name: "Bird",image: "images/bird1.png", price: 3000.0, location: "Mdpur", age: "1 years", isFavorite: true),
-  Features(name: "Cat",image: "images/cat1.png", price: 5000.0, location: "Mdpur", age: "2 years", isFavorite: false),
-  Features(name: "Cat",image: "images/cat1.png", price: 5000.0, location: "Mdpur", age: "2 years", isFavorite: true),
-  Features(name: "Cat",image: "images/cat1.png", price: 5000.0, location: "Mdpur", age: "2 years", isFavorite: true),
+  Features(name: "Dog",image: "images/dog.png", price: 8000.0, location: "Gulshan", age: "3 years", isFavorite: false),
+  Features(name: "Bird",image: "images/bird1.png", price: 3000.0, location: "Dhanmondi", age: "1 years", isFavorite: true),
+  Features(name: "Rabbit",image: "images/rabbit1.png", price: 7000.0, location: "Katabon", age: "2 years", isFavorite: false),
+  Features(name: "Fish",image: "images/fish1.png", price: 1000.0, location: "Krishi Market", age: "1 years", isFavorite: true),
+  Features(name: "Duck",image: "images/duck1.png", price: 1500.0, location: "Rayer Bazar", age: "1 years", isFavorite: true),
   Features(name: "Cat",image: "images/cat1.png", price: 5000.0, location: "Mdpur", age: "2 years", isFavorite: true),
   Features(name: "Cat",image: "images/cat1.png", price: 5000.0, location: "Mdpur", age: "2 years", isFavorite: true),
   Features(name: "Cat",image: "images/cat1.png", price: 5000.0, location: "Mdpur", age: "2 years", isFavorite: true),
