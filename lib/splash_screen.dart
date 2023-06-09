1ad585ae84a42cd822af99f4c1110e44034c98bb
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-09 12:26:32 +0600

    updates designs #7

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:pet_shop/model/category.dart';
 import 'package:pet_shop/model/features.dart';
+import 'package:pet_shop/pages/categories_list.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -336,53 +337,7 @@ class SecondScreen extends StatelessWidget {
     );
   }
 
-  showCategoryList() {
-    return SizedBox(
-      height: 70,
-      child: ListView.builder(
-          shrinkWrap: true,
-          scrollDirection: Axis.horizontal,
-          padding: const EdgeInsets.all(8),
-          itemCount: categoryList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(40),
-              ),
-              child: Container(
-                  width: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    // <= No more error here :)
-                    color: Colors.purple.withOpacity(.4),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(40.0),
-                        child: Container(
-                          color: Colors.white,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Image.asset(
-                              categoryList[index].image.toString(),
-                              height: 30.0,
-                              width: 30.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 8,),
-                      Text(categoryList[index].name.toString()),
-                    ],
-                  )
-              ),
-            );
-          }
-      ),
-    );
-  }
+
 
   showFeaturesList() {
     return SizedBox(
