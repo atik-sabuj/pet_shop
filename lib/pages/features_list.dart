8d9294b94eadbdb1569e056ccf5ab501051afaab
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-09 12:32:18 +0600

    updates designs #8

---
+++ lib/pages/features_list.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:pet_shop/model/features.dart';
+
+showFeaturesList() {
+  return SizedBox(
+    height: 220,
+    child: ListView.separated(
+      shrinkWrap: true,
+      scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.all(8),
+      itemCount: featuresList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Column(
+          children: [
+            Card(
+
+              child: Container(
+                height: 180,
+                width: 220,
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Container(
+                          color: Colors.white,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              featuresList[index].image.toString(),
+                              height: 80.0,
+                              // width: 180.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24,
+                        width: 220,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(featuresList[index].name.toString()),
+                            SizedBox(width: 8,),
+                            Text(featuresList[index].name.toString()),
+
+                          ],
+                        ),
+                      ),
+                      Text(featuresList[index].name.toString()),
+                      Text(featuresList[index].name.toString()),
+                      // featuresList[index].isFavorite==true ? Icon(Icons.ac_unit_sharp) : SizedBox(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      }, separatorBuilder: (BuildContext context, int index) {
+      return Divider(
+        height: 8,
+        color: Colors.grey,
+      );
+    },
+    ),
+  );
+}
+
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:pet_shop/model/category.dart';
 import 'package:pet_shop/model/features.dart';
 import 'package:pet_shop/pages/categories_list.dart';
+import 'package:pet_shop/pages/features_list.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -72,7 +73,10 @@ class SecondScreen extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       SizedBox(
-                        width: MediaQuery.of(context).size.width - 90,
+                        width: MediaQuery
+                            .of(context)
+                            .size
+                            .width - 90,
                         child: TextField(
                           textAlign: TextAlign.start,
                           keyboardType: TextInputType.text,
@@ -336,79 +340,9 @@ class SecondScreen extends StatelessWidget {
       ),
     );
   }
-
-
-
-  showFeaturesList() {
-    return SizedBox(
-      height: 220,
-      child: ListView.separated(
-          shrinkWrap: true,
-          scrollDirection: Axis.horizontal,
-          padding: const EdgeInsets.all(8),
-          itemCount: featuresList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Column(
-              children: [
-                Card(
-
-                  child: Container(
-                    height: 180,
-                    width: 220,
-                    color: Colors.white,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(20.0),
-                            child: Container(
-                              color: Colors.white,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Image.asset(
-                                  featuresList[index].image.toString(),
-                                  height: 80.0,
-                                  // width: 180.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 24,
-                            width: 220,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(featuresList[index].name.toString()),
-                                SizedBox(width: 8,),
-                                Text(featuresList[index].name.toString()),
-
-                              ],
-                            ),
-                          ),
-                          Text(featuresList[index].name.toString()),
-                          Text(featuresList[index].name.toString()),
-                         // featuresList[index].isFavorite==true ? Icon(Icons.ac_unit_sharp) : SizedBox(),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            );
-          }, separatorBuilder: (BuildContext context, int index) {
-            return Divider(
-              height: 8,
-              color: Colors.grey,
-            );
-      },
-      ),
-    );
-  }
 }
+
+
+
 
 
